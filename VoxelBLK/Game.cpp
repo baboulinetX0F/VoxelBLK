5b4f3f5eccb4fee07f1e6d4a87aeee59281e0236
baboulinet <vergilotaku@live.fr>
2016-11-07 14:18:57 +0100

    added Shader class

--- VoxelBLK/Game.cpp
+++ VoxelBLK/Game.cpp
@@ -20,6 +20,24 @@ void Game::init()
 {
 	_renderer = new Renderer();
 	_renderer->initWindow("VoxelBLK", 800, 600);
+
+	// Render test
+	GLfloat data[]
+	{
+		-0.5f,-0.5f,1.0f,
+		-0.5f,0.5f,1.0f,
+		0.5f,0.5f,1.0f
+	};
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 
 void Game::gameLoop()
---
+++ VoxelBLK/Shader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <GL\glew.h>
+
+class Shader
+{
+public:
+	GLuint _program;	// Id of the shader program
+	Shader(const GLchar* vertexPath, const GLchar* fragPath);	// Constructor
+	void Use();
+};
